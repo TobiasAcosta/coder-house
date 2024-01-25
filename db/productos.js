155b7e5eb12e734fdce7b03f3f2e61373a9b95c3
TobiasAcosta <tobiasacostadasd@gmail.com>
2024-01-24 23:32:01 -0300

    ecommerceAcosta

--- db/productos.js
+++ db/productos.js
@@ -2,58 +2,58 @@ export const productos = [
     {
       id: 1,
       nombre: "Mouse",
-      precio: 27000,
+      precio: 27.000,
       imagen:"https://th.bing.com/th/id/OIP.869ILPhR9wrmclvt5Gay0gHaHa?w=177&h=180&c=7&r=0&o=5&pid=1.7",
       categoria:"perisfericos"
     },
     {
       id: 2,
       nombre: "Teclado mecanico",
-      precio: 40000,
+      precio: 40.000,
       imagen:"https://th.bing.com/th?q=Teclado+Mecanico+Bluetooth&w=120&h=120&c=1&rs=1&qlt=90&cb=1&pid=InlineBlock&mkt=es-AR&cc=AR&setlang=es&adlt=moderate&t=1&mw=247",
       categoria:"perisfericos"
     },
     {
       id: 3,
       nombre: "Auriculares gamer",
-      precio: 50000,
+      precio: 50.000,
       image:"https://th.bing.com/th/id/OIP.2IRz7IGX9pv15KQjbf95mgHaHa?w=219&h=219&c=7&r=0&o=5&pid=1.7",
       categoria:"perisfericos"
     },
     {
       id: 4,
       nombre: "monitor 240hz",
-      precio: 800000,
+      precio: 800.000,
       imagen:"https://th.bing.com/th/id/OIP.AMBgxQkYbxYvL4OUAIsONQHaHa?w=202&h=202&c=7&r=0&o=5&pid=1.7",
       categoria:"monitores"
     },
     {
       id: 5,
-      nombre: "goma de borrar blanca",
-      precio: 50,
-      imagen:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYUTI-H3XP-vCd-6v90jROYKlBW3IYK2k7t21whwU&s",
-      categoria:"gomas"
+      nombre: "Procesador AMD Ryzen 5 5600X 4.6GHz Turbo AM4 + Wraith Stealth Cooler",
+      precio: 299.900,
+      imagen:"https://th.bing.com/th/id/OIP.HGbNQK4xQeXF_uikQcvvdwHaFk?w=249&h=187&c=7&r=0&o=5&pid=1.7",
+      categoria:"componentes"
     },
     {
       id: 6,
-      nombre: "Cartuchera Spiderman",
-      precio: 350,
-      imagen:"https://www.coopehogar.coop/media/ch/publico/articulos/a/8/2/a821d9b7fd2a671d2f3652cb2dae33da",
-      categoria:"cartucheras"
+      nombre: "Mother ASUS PRIME A520M-K DDR4 AM4",
+      precio: 104.000,
+      imagen:"https://th.bing.com/th/id/OIP.hy6vYrKRoCsir_5A7Uc4agHaHa?w=208&h=208&c=7&r=0&o=5&pid=1.7",
+      categoria:"componentes"
     },
     {
       id: 7,
-      nombre: "Cartuchera Cars",
-      precio: 350,
-      imagen:"https://www.oxfordlibreria.com.ar/media/catalog/product/cache/5a38f6614905178fa07804facc7b33a0/9/2/92626374160_20211005140012.jpg",
-      categoria:"cartuchera"
+      nombre: "Placa de Video XFX Radeon RX 6650 XT 8GB GDDR6 Speedster SWFT 210",
+      precio: 440.000,
+      imagen:"https://th.bing.com/th/id/OIP.adFc-0hQHkBt6JeI_GxnNAHaHZ?w=172&h=180&c=7&r=0&o=5&pid=1.7",
+      categoria:"componentes"
     },
     {
       id: 8,
-      nombre: "Regla 30cm",
-      precio: 150,
-      imagen:"https://www.paitis.ar/ps/1921-large_default/regla-30-cm.jpg",
-      categoria:"reglas"
+      nombre: "Silla Gamer HHGears SM-115 Negro y Azul (Peso MAX. 90kg) ",
+      precio: 115.850,
+      imagen:"https://th.bing.com/th/id/OIP.0Rwqjuau_NP_96YOnLN48gHaHa?w=184&h=184&c=7&r=0&o=5&pid=1.7",
+      categoria:"sillas"
     }
   ];
   
